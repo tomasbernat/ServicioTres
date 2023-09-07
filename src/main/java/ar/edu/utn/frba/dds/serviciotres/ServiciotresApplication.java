46b51e4cd76c0d5419385be5c5dc2225cb4aaf9c
tomasbernat <tbernatlavalle@frba.utn.edu.ar>
2023-09-07 19:36:42 -0300

    initial commit

---
+++ src/main/java/ar/edu/utn/frba/dds/serviciotres/ServiciotresApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.utn.frba.dds.serviciotres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiciotresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiciotresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/edu/utn/frba/dds/serviciotres/ServiciotresApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.utn.frba.dds.serviciotres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiciotresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
